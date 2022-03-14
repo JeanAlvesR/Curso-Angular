d132e20408046695549f3889157b35c22fbc1c96
Jean Alves Rocha <96813984+JeanAlvesR@users.noreply.github.com>
2022-03-14 13:24:30 -0300

    Primeiro commit

---
+++ data-binding/src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { DataBindingComponent } from './data-binding/data-binding.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { TooltipModule } from 'ngx-bootstrap/tooltip';
+import { InputPropertyComponent } from './input-property/input-property.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    DataBindingComponent,
+    InputPropertyComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    TooltipModule.forRoot(),
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ data-binding/src/app/data-binding/data-binding.component.ts
@@ -0,0 +1,64 @@
+import { Component, OnInit } from '@angular/core';
+import { setTheme } from 'ngx-bootstrap/utils';
+
+@Component({
+  selector: 'app-data-binding',
+  templateUrl: './data-binding.component.html',
+  styleUrls: ['./data-binding.component.css']
+})
+export class DataBindingComponent implements OnInit {
+
+  url: string = 'http://JeanAlves.com';
+  cursoAngular: boolean = true;
+
+  urlImagem2: string = 'https://media.nu.nl/m/3yrxr70asrqb_sqr256.jpg/teruggekeerde-ronaldo-old-trafford-voor-mij-altijd-magische-plek-geweest.jpg';
+  urlImagem: string = 'https://picsum.photos/200';
+
+  valorAtual: string = '';
+  valorSalvo: string= '';
+
+  isMouseOver: boolean = false;
+
+  name: string = '';
+
+  nomeDoCurso: string = 'Angular';
+
+  person: any = {
+
+    nome: 'Jean',
+    idade: 20
+  }
+
+  getValor() {
+    return 1;
+  }
+
+  getCurtirCurso() {
+    return true;
+  }
+
+  constructor() { 
+
+    setTheme('bs4');
+  }
+
+  ngOnInit(): void {
+  }
+
+  botaoClicado() {
+    alert('Botão clicado!');
+  }
+
+  onKeyUp(evento: KeyboardEvent) { 
+    this.valorAtual =(<HTMLInputElement>evento.target).value;
+  }
+
+  salvarValor(valor: string) {
+    this.valorSalvo = valor;
+  }
+
+  onMouseOverOut() {
+    this.isMouseOver = !this.isMouseOver;
+  }
+
+}
