850965e66267ca13a67edba6d1bd9a2e605e21bc
Jean Alves Rocha <96813984+JeanAlvesR@users.noreply.github.com>
2022-03-18 15:00:49 -0300

    Forma feia de pegar o parametro

--- rotas/src/app/app-routing.module.ts
+++ rotas/src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule, Component, ModuleWithProviders } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CursoDetalheComponent } from './curso-detalhe/curso-detalhe.component';
 
 import { CursosComponent } from './cursos/cursos.component';
 import { HomeComponent } from './home/home.component';
@@ -9,6 +10,7 @@ const routes: Routes = [
 {path: '', component: HomeComponent},
 {path: 'login', component: LoginComponent},
 {path: 'cursos', component: CursosComponent},
+{path: 'cursos/:id', component: CursoDetalheComponent},
 ];
 
 export const routing: ModuleWithProviders <RouterModule> = RouterModule.forRoot(routes);
